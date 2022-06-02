70d210fb6f31afab323952127974806bd58666b7
rogeryoungh <rogeryoungh@foxmail.com>
2022-06-02 08:19:48 +0800

    fix: avatar

---
+++ scripts/build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# 预处理 Markdown 文件
+wget https://github.com/rogeryoungh/preprocessor-md-tex/releases/latest/download/preprocessor-md-tex
+chmod +x preprocessor-md-tex
+./preprocessor-md-tex ./content
+
+# Hugo
+cd hugo
