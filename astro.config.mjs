5c411ca63b6a5176aee8d151f9aa4fd666fc079c
rogeryoungh <rogeryoungh@foxmail.com>
2024-10-01 18:35:21 +0800

    update: add katex support

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,15 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 
 import sitemap from '@astrojs/sitemap';
+import rehypeKatex from 'rehype-katex';
+import remarkMath from 'remark-math';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://blog.rogery.dev/',
 	integrations: [mdx(), sitemap()],
+	markdown: {
+		rehypePlugins: [rehypeKatex],
+		remarkPlugins: [remarkMath]
+	}
 });
