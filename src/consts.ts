ede508e0f70833e90de8ee28cffb8ac03f790215
rogeryoungh <42564206+rogeryoungh@users.noreply.github.com>
2024-10-13 15:19:26 +0800

    update: add follow id

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Roger Young';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_DESCRIPTION = 'Welcome to my website! feedId:66145059857881088+userId:41459891222051840';
